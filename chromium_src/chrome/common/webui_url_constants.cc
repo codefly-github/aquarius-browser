4f56e8d866247f13f307c5d6a931aca97c42954f
Codefly <codefly-github@pm.me>
2024-08-06 05:21:46 -0700

    Add chromium_src

---
+++ chromium_src/chrome/common/webui_url_constants.cc
@@ -0,0 +1,24 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/constants/webui_url_constants.h"
+#include "chrome/common/webui_url_constants.h"
+
+#define kChromeUIAttributionInternalsHost                                     \
+  kChromeUIAttributionInternalsHost, kAdblockHost, kAdblockInternalsHost,     \
+      kRewardsPageHost, kRewardsInternalsHost, kWelcomeHost, kWalletPageHost, \
+      kTorInternalsHost, kSkusInternalsHost
+#define kPerformanceSubPage kPerformanceSubPage_UnUsed
+
+#include "src/chrome/common/webui_url_constants.cc"
+
+#undef kPerformanceSubPage
+#undef kChromeUIAttributionInternalsHost
+
+namespace chrome {
+
+const char kPerformanceSubPage[] = "system";
+
+}  // namespace chrome
